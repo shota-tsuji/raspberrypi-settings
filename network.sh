0ed8266590509559963bec70bb8b00c50b83f219
shota-tsuji <shotatuzi@gmail.com>
2022-12-18 10:42:04 +0900

    feat: network (#3)

---
+++ network.sh
@@ -0,0 +1,11 @@
+
+iw dev interface scan
+iw dev wlan0 scan
+sudo iw dev wlan0 scan
+
+ip link show wlan0
+iw dev wlan0 link
+iw dev wlan0 connect Buffalo-G-F088
+sudo iw dev wlan0 connect Buffalo-G-F088
+iw dev wlan0 link
+ip link show wlan0
